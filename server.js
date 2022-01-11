7a4d12e6257c42e2c89945637612dc2721fea9d9
blueprint-fred <frederick.g@blueprintbusinesssolutionscorp.com>
2022-01-12 06:14:05 +0800

    added cors

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ require('./config/database').connect()
 
 const express = require('express');
 const app = express()
+const cors = require('cors')
 
 const port = 1337
 
 app.use(express.json())
+app.use(cors())
 
 const usersRoute = require('./routes/users')
 app.use('/users', usersRoute)
