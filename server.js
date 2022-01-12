34de33f2b184c97506e5db416d8d3c23142dbdde
blueprint-fred <frederick.g@blueprintbusinesssolutionscorp.com>
2022-01-12 08:41:44 +0800

    added for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors')
 
-const port = 1337
+const port = process.env.PORT || 1337
 
 app.use(express.json())
 app.use(cors())
