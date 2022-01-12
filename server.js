2c1fc3d37742166e02d04145312444abd360c329
blueprint-fred <frederick.g@blueprintbusinesssolutionscorp.com>
2022-01-12 08:08:43 +0800

    added stripe

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ const port = 1337
 app.use(express.json())
 app.use(cors())
 
+// users
 const usersRoute = require('./routes/users')
 app.use('/users', usersRoute)
+
+// stripe
+const stripeRoute = require('./routes/stripe')
+app.use('/stripe', stripeRoute)
+
 
 
 app.listen(port, () => console.log(`Runningg on port ${port}`))
